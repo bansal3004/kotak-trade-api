585727a10224a30eca276c4243ef22753ca22fd3
bansal3004 <anujbansal3004@gmail.com>
2025-11-11 10:50:48 +0530

    Initial commit 12

--- secure.php
+++ secure.php
@@ -6,16 +6,16 @@ return [
     'pin' => '1234',
 
     // Kotak API keys
-    'access_token' => '2d34b02f-a084-4aad-96a8-4dbfdf061e33',
-    'ucc' => 'W914F',
-    'mobile' => '+919876625339',
-    'mpin' => '880022' 
+    // 'access_token' => '2d34b02f-a084-4aad-96a8-4dbfdf061e33',
+    // 'ucc' => 'W914F',
+    // 'mobile' => '+919876625339',
+    // 'mpin' => '880022' 
     
       // Kotak API keys
-    // 'access_token' => '949468a6-1b5e-4505-8eb1-d1e56406cb43',
-    // 'ucc' => 'W4MB1',
-    // 'mobile' => '+919810891020',
-    // 'mpin' => '880022' 
+    'access_token' => '949468a6-1b5e-4505-8eb1-d1e56406cb43',
+    'ucc' => 'W4MB1',
+    'mobile' => '+919810891020',
+    'mpin' => '880022' 
 ];
 ?>
 
